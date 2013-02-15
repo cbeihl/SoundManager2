e809a3b56edfad1908c4b1330396c45c8aa64b36
cbeihl <chris.beihl@gmail.com>
2013-02-15 14:35:42 -0600

    add default setup file soundmanager2-setup.js and add troubleshoot folder to artifact zip

---
+++ script/soundmanager2-setup.js
@@ -0,0 +1,30 @@
+/**
+ * soundmanager2-setup.js
+ *
+ * A default setup script for configuring SoundManager2.  This script must be imported into your page after soundmanager2.js
+ *
+ */
+
+soundManager.setup({
+    url: 'sm2/swf',
+    flashVersion: 9,
+    debugFlash: false,
+    debugMode: false,
+    flashLoadTimeout: 0,
+    onready: function() {
+        console.log('SM2 Ready!');
+    },
+    ontimeout: function() {
+        console.log('SM2 timed out on startup...');
+    },
+    defaultOptions: {
+        onfailure: function(sndObj, msg, level, code) {
+            console.log('onfailure() : msg = ' + msg + ', level = ' + level + ', code = ' + code);
+        }
+    },
+    flash9Options: {
+        usePeakData: false,
+        useWaveformData: false,
+        useEqData: false
+    }
+});
